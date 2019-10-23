687aa67a668908d793831fe78cb8453f1c5cc781
Aurea Soriano <aurea.soriano@gmail.com>
2019-10-23 16:34:52 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,19 @@
+'''
+Created on Oct 11, 2019
+
+@author: aurea
+'''
+
+import sys
+import math
+
+class AppWindow():
+    def __init__(self, *args, **kwargs):
+        print("aurea")
+
+   
+
+ 
+
+if __name__ == "__main__":
+    w = AppWindow()
